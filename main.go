e9af3862af6c6f0a8edce15824dd3cacd9e79456
Juan C. Müller <jcmuller@gmail.com>
2023-02-23 14:37:07 -0500

    Add version

--- main.go
+++ main.go
@@ -5,10 +5,25 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/spf13/pflag"
 	"golang.org/x/exp/slog"
 )
 
+var (
+	version = pflag.BoolP("version", "V", false, "Print version number")
+)
+
 func main() {
+	pflag.Parse()
+
+	if *version {
+		if err := printVersion(); err != nil {
+			slog.Error("error retrieving version information", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
 	ctx := context.Background()
 	token, err := retrieveToken(ctx)
 	if err != nil {
